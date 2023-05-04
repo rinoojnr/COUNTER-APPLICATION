be0600abbef381dbb794469329486c9945d29a17
Rinooj N R <rinoojnr@gmail.com>
2023-05-04 20:12:55 +0530

    Added changes in counter app css

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -15,11 +15,10 @@ function App() {
       <div className='counter-container'>
         <div className='header'>
           <h1 className='head'>Counter App</h1>
-          <h2 className='sub' >Counter:{state}</h2>
-          <div className='button-container'>
-            <button className='plus-button' onClick={increase}>+</button>
-            <button className='minus-button' onClick={decrease} >-</button>
-          </div>
+          <h2 className='sub' >{state}</h2>
+          <div className='plus-button' onClick={increase}>+</div>
+          <div className='minus-button' onClick={decrease} >-</div>
+          <div className='plus-five-button'>+5</div>
         </div>
       </div>  
     </div>
